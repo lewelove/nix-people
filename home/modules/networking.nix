371a0d465a953efa24b2b89dce36e9ba2163120b
lewelove <lewelove@proton.me>
2026-03-18 15:53:18 +0300

    2026-03-18 at 12:53 UTC

--- home/modules/networking.nix
+++ home/modules/networking.nix
@@ -7,9 +7,6 @@
     firewall.checkReversePath = "loose";
     firewall.allowedTCPPorts = [ 80 8080 6600 666 ];
     firewall.allowedUDPPorts = [ 9 ]; 
-
-    interfaces.enp3s0.wakeOnLan.enable = true;
-
   };
 
   environment.systemPackages = [ pkgs.ethtool ];
