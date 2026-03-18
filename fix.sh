e611c38a9e9e0cc7fb2387a040ab55bb171ada6f
lewelove <lewelove@proton.me>
2026-03-18 15:22:49 +0300

    2026-03-18 at 12:22 UTC

--- fix.sh
+++ fix.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko -- --mode mount home/disko.nix
-
-cd /mnt/home/romma/nix-hosts/home
+cd /mnt/home/romma/nix-config/home
 
 nixos-generate-config --no-filesystems --show-hardware-config > modules/hardware-configuration.nix
 
